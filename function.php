4ed03cea20fde3c157d86ccbe34e3fdf3b16a9eb
kabayan349 <67649284+kabayan349@users.noreply.github.com>
2020-07-15 12:41:57 +0700

    asass

--- function.php
+++ function.php
@@ -7,12 +7,11 @@ $header[] = "User-Agent: okhttp/3.10.0";
 $header[] = "Accept: application/json";
 $header[] = "Accept-Language: id-ID";
 $header[] = "Content-Type: application/json; charset=UTF-8";
-$header[] = "X-AppVersion: 3.30.2";
+$header[] = "X-AppVersion: 3.46.1";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -6.917464,107.619122";
-$header[] = "X-Location-Accuracy: 3.0";
+$header[] = "X-Location: -5.1385".mt_rand(100,999).",119.418".mt_rand(1000,9999);
 if ($pin):
 $header[] = "pin: $pin";
     endif;
